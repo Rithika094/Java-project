8130e19ce257e520f678c01897ebb79f47473c8b
Rithika Gopalakrishnan <rithikag094@gmail.com>
2026-07-25 10:53:11 +0530

    Add files via upload

---
+++ Topolo.java
@@ -0,0 +1,49 @@
+
+import java.util.*;
+
+public class Topolo {
+    public static void main(String[] args) {
+        int vertices = 6;
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < vertices; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        graph.get(5).add(2);
+        graph.get(5).add(0);
+        graph.get(4).add(0);
+        graph.get(4).add(1);
+        graph.get(2).add(3);
+        graph.get(3).add(1);
+        topologicalSort(graph, vertices);
+    }
+
+    static void topologicalSort(ArrayList<ArrayList<Integer>> graph, int n) {
+        int[] indegree = new int[n];
+        for (int i = 0; i < n; i++) {
+            for (int neighbour : graph.get(i)) {
+                indegree[neighbour]++;
+            }
+        }
+
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                queue.add(i);
+            }
+        }
+        System.out.println("Topological Order:");
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            System.out.print(current + " ");
+            for (int neighbour : graph.get(current)) {
+                indegree[neighbour]--;
+                if (indegree[neighbour] == 0) {
+                    queue.add(neighbour);
+                }
+            }
+        }
+    }
+}
